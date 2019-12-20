47589ff13a2d06ee3b2d6e9891f854b3333f2e8f
Kinga Janicka <kingajanicka7@gmail.com>
2019-12-20 14:37:01 +0000

    aaa

--- anameforafolder/src/index.js
+++ anameforafolder/src/index.js
@@ -1,7 +1,7 @@
 // const links = document.querySelectorAll("nav ul li");
 // console.log(links);
 
-// const bgs = ["violet", "yellow", "purple", "pink", "blue"];
+const colours = ["violet", "yellow", "purple", "pink", "blue"];
 
 // function linkColour(ts) {
 //   if (Date.now() - ts > 1000) {
@@ -79,3 +79,52 @@ mainText.forEach(headerText => {
   h.textContent = headerText.content;
   main.appendChild(h);
 });
+
+const mainHeader = document.createElement("h1");
+main.appendChild(mainHeader);
+mainHeader.textContent = "Hello";
+
+mainHeader.addEventListener("mouseover", e => {
+  e.target.style.color = "purple";
+  console.log(e);
+});
+
+mainHeader.addEventListener("mouseout", e => {
+  e.target.style.color = "black";
+  console.log(e);
+});
+
+let i = 0;
+const token = setInterval(() => {
+  if (i < colours.length) {
+    mainHeader.style.color = colours[i];
+    i++;
+  } else {
+    i = 0;
+    //clearInterval(token);
+  }
+}, 100);
+
+const title = document.createElement("h1");
+header.appendChild(title);
+title.textContent = "Hi";
+title.style.color = "#00beef";
+
+(async () => {
+  // const value = await new Promise((resolve, reject) => {
+  //   setTimeout(() => {
+  //     resolve("Hi");
+  //   }, 5000);
+  // });
+  // alert(value);
+
+  const result = await fetch("https://httpbin.org/get");
+  const data = await result.json();
+  console.log(data);
+
+  console.log(data.headers);
+})();
+
+fetch("https://httpbin.org/get")
+  .then(res => res.json())
+  .then(console.log);
